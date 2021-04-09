03f1aa9c68073afa4bfc7f9c5b56357cafd63c61
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-04-09 17:37:07 +0300

    Update Modules.js

--- Modules.js
+++ Modules.js
@@ -58,7 +58,7 @@ verifyLoginCredentials = (username, password, rememberMe, postResponse) =>
                         else // if the passwords don't match
                         {
                             console.log("Sorry, the passwords don't match"); // testing
-                            postResponse.redirect("/"); // redirecting back to the start route
+                            postResponse.redirect("/incorrect-credentials"); // redirecting to the incorrect credentials route
                         }
                     });
                 })
@@ -66,7 +66,7 @@ verifyLoginCredentials = (username, password, rememberMe, postResponse) =>
             else // if the username hasn't been found
             {
                 console.log("The username doesn't exist"); // testing
-                postResponse.redirect("/"); // redirecting back to the start route
+                postResponse.redirect("/incorrect-credentials"); // redirecting to the incorrect credentials route
             }
         });
     });
@@ -85,7 +85,7 @@ verifyRegisterCredentials = (username, password, postResponse) =>
             if(number!=0) // if the username has been found
             {
                 console.log("The username already exists"); // testing
-                postResponse.redirect("/");
+                postResponse.redirect("/incorrect-credentials"); // redirecting to the incorrect credentials route
             }
             else // if the username doesn't exist into the database
             {
@@ -102,6 +102,42 @@ verifyRegisterCredentials = (username, password, postResponse) =>
                     });
                 });         
             }
+        });
+    });
+}
+
+recoverPassword = (username, newPassword, confirmPassword, postResponse) =>
+{
+    mongoClient.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => // creating a connection to the database
+    {
+        const dbObject=db.db("loginDB"); // database name
+
+        assert.strictEqual(null, err);
+
+        dbObject.collection("users").find({username: username}).limit(1).count().then((number) => // searching the username in the database
+        {
+            if(number!=0) // if the username has been found
+            {
+                bcrypt.hash(newPassword, saltRounds, (error, newPasswordHash) =>
+                {
+                    if(newPassword===confirmPassword) // if the two typed passwords match
+                    {
+                        console.log("The passwords are the same");
+                        dbObject.collection("users").updateOne({username: username}, {$set: {password: newPasswordHash}}, (error1, result) => // updating user's password
+                        {
+                            assert.strictEqual(null, error1);
+                            console.log("Password updated successfully");
+                            postResponse.redirect("/login"); // redirecting to the login route
+                        });
+                    }
+                    else
+                    {
+                        console.log("The passwords are NOT the same");
+                        postResponse.redirect("/passwords-dont-match"); // redirecting to the passwords don't match route
+                    }
+                });
+            }
+            else postResponse.redirect("/incorrect-credentials"); // if the username doesn't exist into the database
         });
     });
 }
@@ -162,10 +198,25 @@ app.get("/logout", (req, res) => // logout route
         res.clearCookie('auth_type');
         res.redirect("/login");
     }
-    else // if not all the cookies exist
+    else // if not all the cookies exist (or no cookie exists at all)
     {
         res.sendFile(path+'/login.html');
     }
+});
+
+app.get("/recover-password", (req, res) =>
+{
+    res.sendFile(path+'/recover.html');
+});
+
+app.get("/incorrect-credentials", (req, res) =>
+{
+    res.render("notFound", {message: "Oops! Incorrect username or password", error: 400});
+});
+
+app.get("/passwords-dont-match", (req, res) =>
+{
+    res.render("notFound", {message: "Sorry! The typed passwords don't match", error: 400});
 });
 
 app.post("/", (req, res) => // start post route
@@ -181,6 +232,16 @@ app.post("/", (req, res) => // start post route
             verifyLoginCredentials(req.body.username, req.body.password, req.body.remember_checkbox, res);
             break;
     }
+});
+
+app.post("/recover-password", (req, res) =>
+{
+    recoverPassword(req.body.username, req.body.new_password, req.body.confirm_password, res);
+});
+
+app.get("*", (req, res) =>
+{
+    res.render("notFound", {message: "Oops! The requested page doesn't exist.", error: 404});
 });
 
 app.listen(port); // starting the server on the specified port
