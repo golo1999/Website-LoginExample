f3b4748c1ba31932a641d43ec1facedd559a22b0
George-Alexandru Gologan <aggologan1999@gmail.com>
2021-07-22 00:33:08 +0300

    Replacing the double quotation marks with simple ones in Modules.js

--- Modules.js
+++ Modules.js
@@ -1,76 +1,65 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cookieParser = require("cookie-parser");
-const noCache = require("nocache");
+const cookieParser = require('cookie-parser');
+const noCache = require('nocache');
 const port = 3000 || process.env.PORT;
-const path = require("path").dirname(require.main.filename);
-const mongo = require("mongodb");
+const path = require('path').dirname(require.main.filename);
+const mongo = require('mongodb');
 const mongoClient = mongo.MongoClient;
-const assert = require("assert");
-const bcrypt = require("bcrypt");
+const assert = require('assert');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const dbURL = "mongodb://localhost:127.0.0.1:27017/users";
-const session = require("express-session");
+const dbURL = 'mongodb://localhost:127.0.0.1:27017/users';
+const session = require('express-session');
 
-app.use("/assets", express.static(__dirname + '/assets'));
+app.use('/assets', express.static(__dirname + '/assets'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: false }));
 app.use(noCache());
 app.use(cookieParser());
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 verifyLoginCredentials = (username, password, rememberMe, postResponse) => {
     // creating a connection to the database
     mongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
         // database name
-        const dbObject = db.db("loginDB");
+        const dbObject = db.db('loginDB');
         assert.strictEqual(null, err);
 
         // searching the username in the database
-        dbObject.collection("users").find({ username: username }).limit(1).count().then((number) => {
+        dbObject.collection('users').find({ username: username }).limit(1).count().then((number) => {
             // if the username has been found
             if (number != 0) {
-                dbObject.collection("users").findOne({ username: username }, (err, result) => {
+                dbObject.collection('users').findOne({ username: username }, (err, result) => {
                     assert.strictEqual(null, err);
 
                     bcrypt.compare(password, result.password, (err3, result2) => {
                         if (result2 === true) {
-                            // postResponse.cookie('user_id', result._id.toString(), rememberMe === 'on' ?
-                            //     { maxAge: 600000, overwrite: true } : { overwrite: true }
-                            // );
+                            postResponse.cookie('user_id', result._id.toString(), rememberMe === 'on' ?
+                                { maxAge: 600000, overwrite: true } : { overwrite: true }
+                            );
 
-                            // postResponse.cookie('username', username, rememberMe === 'on' ?
-                            //     { maxAge: 600000, overwrite: true } : { overwrite: true }
-                            // );
+                            postResponse.cookie('username', username, rememberMe === 'on' ?
+                                { maxAge: 600000, overwrite: true } : { overwrite: true }
+                            );
 
-                            // postResponse.cookie('auth_type', 'login', rememberMe === 'on' ?
-                            //     { maxAge: 600000, overwrite: true } : { overwrite: true }
-                            // );
-
-                            if (rememberMe === "on") {
-                                postResponse.cookie('user_id', result._id.toString(), { maxAge: 600000, overwrite: true });
-                                postResponse.cookie('username', username, { maxAge: 600000, overwrite: true });
-                                postResponse.cookie('auth_type', 'login', { maxAge: 600000, overwrite: true });
-                            }
-                            else {
-                                postResponse.cookie('user_id', result._id.toString(), { overwrite: true });
-                                postResponse.cookie('username', username, { overwrite: true });
-                                postResponse.cookie('auth_type', 'login', { overwrite: true });
-                            }
+                            postResponse.cookie('auth_type', 'login', rememberMe === 'on' ?
+                                { maxAge: 600000, overwrite: true } : { overwrite: true }
+                            );
 
                             // redirecting to the homepage route
-                            postResponse.redirect("/home");
+                            postResponse.redirect('/home');
                         }
                         // if the passwords don't match, redirecting to the incorrect credentials route
                         else {
-                            postResponse.redirect("/incorrect-credentials");
+                            postResponse.redirect('/incorrect-credentials');
                         }
                     });
                 })
             }
             // if the username hasn't been found, redirecting to the incorrect credentials route
             else {
-                postResponse.redirect("/incorrect-credentials");
+                postResponse.redirect('/incorrect-credentials');
             }
         });
     });
@@ -80,26 +69,26 @@ verifyRegisterCredentials = (username, password, postResponse) => {
     // creating a connection to the database
     mongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
         // database name
-        const dbObject = db.db("loginDB");
+        const dbObject = db.db('loginDB');
 
         assert.strictEqual(null, err);
 
         // searching the username in the database
-        dbObject.collection("users").find({ username: username }).limit(1).count().then((number) => {
+        dbObject.collection('users').find({ username: username }).limit(1).count().then((number) => {
             // if the username has been found, redirecting to the incorrect credentials route
             if (number != 0) {
-                postResponse.redirect("/incorrect-credentials");
+                postResponse.redirect('/incorrect-credentials');
             }
             // if the username doesn't exist into the database
             else {
                 bcrypt.hash(password, saltRounds, (error, hash) => {
                     // inserting the new user
-                    dbObject.collection("users").insertOne({ username: username, password: hash }, (err1, result) => {
+                    dbObject.collection('users').insertOne({ username: username, password: hash }, (err1, result) => {
                         assert.strictEqual(null, err1);
                         postResponse.cookie('auth_type', 'register', { overwrite: true });
                         postResponse.cookie('username', username, { overwrite: true });
                         // redirecting to the homepage route
-                        postResponse.redirect("/home");
+                        postResponse.redirect('/home');
                         // closing the connection to the database
                         db.close();
                     });
@@ -113,75 +102,75 @@ recoverPassword = (username, newPassword, confirmPassword, postResponse) => {
     // creating a connection to the database
     mongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
         // database name
-        const dbObject = db.db("loginDB");
+        const dbObject = db.db('loginDB');
 
         assert.strictEqual(null, err);
 
         // searching the username in the database
-        dbObject.collection("users").find({ username: username }).limit(1).count().then((number) => {
+        dbObject.collection('users').find({ username: username }).limit(1).count().then((number) => {
             // if the username has been found
             if (number != 0) {
                 bcrypt.hash(newPassword, saltRounds, (error, newPasswordHash) => {
                     // if the two typed passwords match
                     if (newPassword === confirmPassword) {
                         // updating user's password
-                        dbObject.collection("users")
+                        dbObject.collection('users')
                             .updateOne({ username: username }, { $set: { password: newPasswordHash } },
                                 (error1, result) => {
                                     assert.strictEqual(null, error1);
                                     // redirecting to the login route
-                                    postResponse.redirect("/login");
+                                    postResponse.redirect('/login');
                                 });
                     }
                     // if the two typed passwords don't match, redirecting to the passwords don't match route
                     else {
-                        postResponse.redirect("/passwords-dont-match");
+                        postResponse.redirect('/passwords-dont-match');
                     }
                 });
             }
             // if the username doesn't exist into the database
             else {
-                postResponse.redirect("/incorrect-credentials");
+                postResponse.redirect('/incorrect-credentials');
             }
         });
     });
 }
 
 // root get route
-app.get("/", (req, res) => {
-    res.redirect("/login");
+app.get('/', (req, res) => {
+    res.redirect('/login');
 });
 
 // home page route
-app.get("/home", (req, res) => {
+app.get('/home', (req, res) => {
     const { cookies } = req;
     // if the two cookies exist
-    if ("username" in cookies && "auth_type" in cookies) {
+    if ('username' in cookies && 'auth_type' in cookies) {
         // sending the variables to the home.ejs to be rendered
-        res.render("home", { username: cookies.username, auth_type: cookies.auth_type });
+        res.render('home', { username: cookies.username, auth_type: cookies.auth_type });
     }
     // if the two cookies doesn't exist
     else {
-        res.redirect("/login");
+        res.redirect('/login');
     }
 });
 
 // login route
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
     const { cookies } = req;
     // if the three cookies exist
-    if ("user_id" in cookies && "username" in cookies && "auth_type" in cookies) {
+    if ('user_id' in cookies && 'username' in cookies && 'auth_type' in cookies) {
         // creating a connection to the database
         mongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
             // database name
-            const dbObject = db.db("loginDB");
+            const dbObject = db.db('loginDB');
 
             assert.strictEqual(null, err);
-            dbObject.collection("users").findOne({ username: cookies.username }, { username: 1 }, (err1, result) => {
+            dbObject.collection('users').findOne({ username: cookies.username }, { username: 1 }, (err1, result) => {
                 assert.strictEqual(null, err1);
                 // if the cookie id is equal to the database user id
                 if (result._id.toString() === cookies.user_id) {
-                    res.redirect("/home");
+                    res.redirect('/home');
                 }
                 else {
                     res.sendFile(path + '/login.html');
@@ -194,14 +183,14 @@ app.get("/login", (req, res) => {
 });
 
 // logout route
-app.get("/logout", (req, res) => {
+app.get('/logout', (req, res) => {
     const { cookies } = req;
     // if the three cookies exist
-    if ("user_id" in cookies && "username" in cookies && "auth_type" in cookies) {
+    if ('user_id' in cookies && 'username' in cookies && 'auth_type' in cookies) {
         res.clearCookie('user_id');
         res.clearCookie('username');
         res.clearCookie('auth_type');
-        res.redirect("/login");
+        res.redirect('/login');
     }
     // if not all the cookies exist (or no cookie exists at all)
     else {
@@ -210,22 +199,22 @@ app.get("/logout", (req, res) => {
 });
 
 // recover password route
-app.get("/recover-password", (req, res) => {
+app.get('/recover-password', (req, res) => {
     res.sendFile(path + '/recover.html');
 });
 
 // incorrect credentials route
-app.get("/incorrect-credentials", (req, res) => {
-    res.render("notFound", { message: "Oops! Incorrect username or password", error: 400 });
+app.get('/incorrect-credentials', (req, res) => {
+    res.render('notFound', { message: 'Oops! Incorrect username or password', error: 400 });
 });
 
 // passwords don't match route
-app.get("/passwords-dont-match", (req, res) => {
-    res.render("notFound", { message: "Sorry! The typed passwords don't match", error: 400 });
+app.get('/passwords-dont-match', (req, res) => {
+    res.render('notFound', { message: "Sorry! The typed passwords don't match", error: 400 });
 });
 
 // root post route
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
     switch (req.body.button) // checking which button was pressed
     {
         case 'Register':
@@ -238,13 +227,13 @@ app.post("/", (req, res) => {
 });
 
 // recover password post route
-app.post("/recover-password", (req, res) => {
+app.post('/recover-password', (req, res) => {
     recoverPassword(req.body.username, req.body.new_password, req.body.confirm_password, res);
 });
 
 // everything else route
-app.get("*", (req, res) => {
-    res.render("notFound", { message: "Oops! The requested page doesn't exist.", error: 404 });
+app.get('*', (req, res) => {
+    res.render('notFound', { message: "Oops! The requested page doesn't exist.", error: 404 });
 });
 
 // starting the server on the specified port
